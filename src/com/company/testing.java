ed0c9fb654b8aacf1da28115ff9f61c69ea6bd7b
khoingx <55979401+khoingx@users.noreply.github.com>
2020-06-15 23:14:19 +1000

    add more dropdown tedt

--- src/com/company/testing.java
+++ src/com/company/testing.java
@@ -17,33 +17,32 @@ public class testing {
         System.setProperty("webdriver.chrome.driver", "/Users/khoinguyen/Downloads/chromedriver");
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //go to the page
         driver.get("https://www.spicejet.com");
-        Thread.sleep(5000);
-
-//        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //click on to open the main dropdown
         driver.findElement(By.id("divpaxinfo")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //select a number of adult
         WebElement element1 = driver.findElement(By.id("ctl00_mainContent_ddl_Adult"));
         Select s1 = new Select(element1);
         s1.selectByValue("1");
-        Thread.sleep(5000);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //select a number of children
         WebElement element2 = driver.findElement(By.id("ctl00_mainContent_ddl_Child"));
         Select s2 = new Select(element2);
         s2.selectByValue("1");
-        Thread.sleep(5000);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+//        Thread.sleep(5000);
 
 
 //        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 
-//        Thread.sleep(2000);
         //click on 1 more dropdown
         WebElement currency = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
         currency.click();
