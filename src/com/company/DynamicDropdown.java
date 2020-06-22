491116e26ee8ad7eafe3ed70e15a69026d99603f
khoingx <55979401+khoingx@users.noreply.github.com>
2020-06-22 22:31:29 +1000

    dynamic dropdown

--- src/com/company/DynamicDropdown.java
+++ src/com/company/DynamicDropdown.java
@@ -9,31 +9,35 @@ import org.testng.annotations.Test;
 
 public class DynamicDropdown {
 
-    private String baseURL;
-    private WebDriver driver;
-    private WebElement element;
+//    private String baseURL = "";
+//    private WebDriver driver;
+//    private WebElement element;
 
     @BeforeTest
-    public void setUp() {
+    public void setUp() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/khoinguyen/Downloads/chromedriver");
 
+//        Thread.sleep(3000);
+    }
+
+    @Test
+    public void dynamicDropdown() throws InterruptedException {
+
         WebDriver driver = new ChromeDriver();
-        baseURL = "https://www.spicejet.com/";
+//        baseURL = "https://www.spicejet.com/";
+        driver.get("https://www.spicejet.com");
         driver.manage().window().maximize();
         Thread.sleep(3000);
-    }
-    @Test
-    public void dynamicDropdown() {
-        driver.get(baseURL);
-        Thread.sleep(3000);
 
+        //click on departure city
         WebElement fromCity = driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT"));
         fromCity.click();
 
-        //select Hyderbad
+        //select Hyderbad as a departure
         driver.findElement(By.xpath("//a[@text='Hyderabad (HYD)']")).click();
         Thread.sleep(2000);
 
+        //select Chennai as an arrival
         //xpath (//a[@value='MAA'])[2]
         driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
 
